f689faa0e77e8b01322bcc24d65db4ed0490c687
Arminsharifi4@gmail.com <Arminsharifi4@gmail.com>
2022-08-30 14:01:31 +0430

    Update

--- MoaiDateTime/ExtensionMethods/DateTimeExtensionMethod.cs
+++ MoaiDateTime/ExtensionMethods/DateTimeExtensionMethod.cs
@@ -10,6 +10,11 @@ namespace MoaiDateTime.ExtensionMethods
             return DateConverter.ChangeGeorgianDateToStringPersianDate(dt);
         }
         
+        public static string ToShamsiDateAndTimeString(this DateTime dt)
+        {
+            return DateConverter.ChangeGeorgianDateToStringPersianDate(dt) + " - " + dt.ToShortTimeString();
+        }
+        
         public static string ToHijriDateString(this DateTime dt)
         {
             return DateConverter.ChangeGeorgianDateToStringPersianDate(dt);
